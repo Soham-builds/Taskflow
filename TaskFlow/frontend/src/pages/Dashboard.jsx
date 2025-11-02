a96ab36f2e6199dd1810ed6493ca79364f300791
Soham Mhadeshwar <97698247+Soham-builds@users.noreply.github.com>
2025-11-02 15:57:45 +0530

    Update Dashboard.jsx

--- TaskFlow/frontend/src/pages/Dashboard.jsx
+++ TaskFlow/frontend/src/pages/Dashboard.jsx
@@ -12,7 +12,7 @@ import {
 } from '../assets/dummy'
 
 // API Base
-const API_BASE = "http://localhost:4000/api/tasks"
+const API_BASE = "https://personaltaskmanager-backend-ewn3.onrender.com/api/tasks"
 
 const Dashboard = () => {
   const { tasks, refreshTasks } = useOutletContext()
@@ -154,3 +154,4 @@ const Dashboard = () => {
 }
 
 export default Dashboard
+
